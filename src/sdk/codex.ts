f4dbea7731be97fe4346fd59db4a055888373560
Arnaud <arnaud@status.im>
2024-09-16 14:30:13 +0200

    Rename variables for better names

--- src/sdk/codex.ts
+++ src/sdk/codex.ts
@@ -1,42 +1,41 @@
 import { Codex } from "@codex-storage/sdk-js";
 import { WebStorage } from "../utils/web-storage";
 
-// TODO rename
-let _client: Codex;
-let _url: string;
+let client: Codex;
+let url: string;
 
 export const CodexSdk = {
   url() {
-    return _url;
+    return url;
   },
 
   load() {
     return WebStorage.get<string>("codex-node-url").then((url) => {
-      _url = url || import.meta.env.VITE_CODEX_API_URL;
-      _client = new Codex(_url);
+      url = url || import.meta.env.VITE_CODEX_API_URL;
+      client = new Codex(url);
     });
   },
 
   updateURL(url: string) {
-    _url = url;
-    _client = new Codex(url);
+    url = url;
+    client = new Codex(url);
 
     return WebStorage.set("codex-node-url", url);
   },
 
   get debug() {
-    return _client.debug;
+    return client.debug;
   },
 
   get data() {
-    return _client.data;
+    return client.data;
   },
 
   get node() {
-    return _client.node;
+    return client.node;
   },
 
   get marketplace() {
-    return _client.marketplace;
+    return client.marketplace;
   },
 };
