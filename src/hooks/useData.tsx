14dac86dabf98bbf5c04d691719c31f85572f685
Arnaud <arnaud@status.im>
2024-09-26 15:53:37 +0200

    Move the 'mock' data into a proxy object while waiting for the API to provide the metadata

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -1,76 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
-import { FilesStorage } from "../utils/file-storage";
 import { CodexSdk } from "../sdk/codex";
-import * as Sentry from "@sentry/browser";
-import { CodexDataContent } from "@codex-storage/sdk-js";
+import { CodexDataResponse } from "@codex-storage/sdk-js";
+import { Promises } from "../utils/promises";
 
 export function useData() {
-  const { data = [] } = useQuery({
-    queryFn: (): Promise<CodexDataContent[]> => {
-      // TODO refactor
-      return Promise.resolve().then(async () => {
-        const res = await CodexSdk.data.cids();
+  const { data = { content: [] } satisfies CodexDataResponse } =
+    useQuery<CodexDataResponse>({
+      queryFn: (_) =>
+        CodexSdk.data.cids().then((res) => Promises.rejectOnError(res)),
+      queryKey: ["cids"],
 
-        if (res.error) {
-          if (import.meta.env.PROD) {
-            Sentry.captureException(res.data);
-          }
-          return [];
-        }
+      initialData: { content: [] } satisfies CodexDataResponse,
 
-        const metadata = await FilesStorage.list();
+      // No need to retry because if the connection to the node
+      // is back again, all the queries will be invalidated.
+      retry: false,
 
-        return res.data.content.map((content, index) => {
-          if (content.manifest.filename) {
-            return content;
-          }
+      // The client node should be local, so display the cache value while
+      // making a background request looks good.
+      staleTime: 0,
 
-          const value = metadata.find(([cid]) => content.cid === cid);
+      // Don't expect something new when coming back to the UI
+      refetchOnWindowFocus: false,
+    });
 
-          if (!value) {
-            return {
-              cid: content.cid,
-              manifest: {
-                ...content.manifest,
-                mimetype: "N/A",
-                uploadedAt: new Date(0, 0, 0, 0, 0, 0).toJSON(),
-                filename: "N/A" + index,
-              },
-            };
-          }
-
-          const {
-            mimetype = "",
-            name = "",
-            uploadedAt = new Date(0, 0, 0, 0, 0, 0).toJSON(),
-          } = value[1];
-
-          return {
-            cid: content.cid,
-            manifest: {
-              ...content.manifest,
-              mimetype,
-              filename: name,
-              uploadedAt: uploadedAt,
-            },
-          };
-        });
-      });
-    },
-    queryKey: ["cids"],
-    initialData: [],
-
-    // No need to retry because if the connection to the node
-    // is back again, all the queries will be invalidated.
-    retry: false,
-
-    // The client node should be local, so display the cache value while
-    // making a background request looks good.
-    staleTime: 0,
-
-    // Don't expect something new when coming back to the UI
-    refetchOnWindowFocus: false,
-  });
-
-  return data;
+  return data.content;
 }
---
+++ src/proxy.ts
@@ -0,0 +1,147 @@
+import {
+  CodexData,
+  CodexDataResponse,
+  CodexMarketplace,
+  SafeValue,
+  UploadResponse,
+} from "@codex-storage/sdk-js";
+import { CodexSdk as Sdk } from "./sdk/codex";
+import { Promises } from "./utils/promises";
+import { WebStorage } from "./utils/web-storage";
+import * as Sentry from "@sentry/browser";
+import { FilesStorage } from "./utils/file-storage";
+
+class CodexDataMock extends CodexData {
+  override upload(
+    file: File,
+    onProgress?: (loaded: number, total: number) => void
+  ): Promise<UploadResponse> {
+    const res = super.upload(file, onProgress);
+
+    return res.then(({ result, abort }) => {
+      return {
+        abort,
+        result: result.then((safe) => {
+          if (!safe.error) {
+            return WebStorage.set(safe.data, {
+              type: file.type,
+              name: file.name,
+            }).then(() => safe);
+          }
+
+          return safe;
+        }),
+      };
+    });
+  }
+
+  override async cids(): Promise<SafeValue<CodexDataResponse>> {
+    const res = await super.cids();
+
+    if (res.error) {
+      return res;
+    }
+
+    const metadata = await FilesStorage.list();
+
+    const content = res.data.content.map((content, index) => {
+      if (content.manifest.filename) {
+        return content;
+      }
+
+      const value = metadata.find(([cid]) => content.cid === cid);
+
+      if (!value) {
+        return {
+          cid: content.cid,
+          manifest: {
+            ...content.manifest,
+            mimetype: "N/A",
+            uploadedAt: new Date(0, 0, 0, 0, 0, 0).toJSON(),
+            filename: "N/A" + index,
+          },
+        };
+      }
+
+      const {
+        mimetype = "",
+        name = "",
+        uploadedAt = new Date(0, 0, 0, 0, 0, 0).toJSON(),
+      } = value[1];
+
+      return {
+        cid: content.cid,
+        manifest: {
+          ...content.manifest,
+          mimetype,
+          filename: name,
+          uploadedAt: uploadedAt,
+        },
+      };
+    });
+
+    return { error: false, data: { content } };
+  }
+}
+
+class CodexMarketplaceMock extends CodexMarketplace {
+  // override createStorageRequest(
+  //   input: CodexCreateStorageRequestInput
+  // ): Promise<SafeValue<string>> {
+  //   return Promise.resolve({
+  //     error: true,
+  //     data: {
+  //       message: "C'est balo",
+  //     },
+  //   });
+  // }
+  // override createAvailability(): Promise<
+  //   SafeValue<CodexAvailabilityCreateResponse>
+  // > {
+  //   return Promise.resolve({
+  //     error: true,
+  //     data: {
+  //       message: "C'est balo",
+  //     },
+  //   });
+  // }
+  // override reservations(): Promise<SafeValue<CodexReservation[]>> {
+  //   return Promise.resolve({
+  //     error: false,
+  //     data: [
+  //       {
+  //         id: "0x123456789",
+  //         availabilityId: "0x12345678910",
+  //         requestId: "0x1234567891011",
+  //         /**
+  //          * Size in bytes
+  //          */
+  //         size: 500_000_000 + "",
+  //         /**
+  //          * Slot Index as hexadecimal string
+  //          */
+  //         slotIndex: "2",
+  //       },
+  //       {
+  //         id: "0x987654321",
+  //         availabilityId: "0x9876543210",
+  //         requestId: "0x98765432100",
+  //         /**
+  //          * Size in bytes
+  //          */
+  //         size: 500_000_000 + "",
+  //         /**
+  //          * Slot Index as hexadecimal string
+  //          */
+  //         slotIndex: "1",
+  //       },
+  //     ],
+  //   });
+  // }
+}
+
+export const CodexSdk = {
+  ...Sdk,
+  marketplace: new CodexMarketplaceMock(import.meta.env.VITE_CODEX_API_URL),
+  data: new CodexDataMock(import.meta.env.VITE_CODEX_API_URL),
+};
