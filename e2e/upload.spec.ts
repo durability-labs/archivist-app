2246d4d78e7ee97641bd9899450622521ddbb441
Arnaud <arnaud@status.im>
2024-10-11 17:58:13 +0200

    Fix lint

--- e2e/upload.spec.ts
+++ e2e/upload.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test';
-import { Buffer } from 'buffer';
 import { readFileSync } from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
--- src/routes/dashboard/peers.tsx
+++ src/routes/dashboard/peers.tsx
@@ -13,90 +13,94 @@ import { CodexSdk } from "../../sdk/codex";
 // This function accepts the same arguments as DottedMap in the example above.
 const mapJsonString = getMapJSON({ height: 60, grid: "diagonal" });
 
+const Peers = () => {
+  const [pins, setPins] = useState<[PeerPin, number][]>([]);
+  const { data } = useQuery({
+    queryFn: () =>
+      CodexSdk.debug()
+        .info()
+        .then((s) => Promises.rejectOnError(s)),
+    queryKey: ["debug"],
+
+    // No need to retry because if the connection to the node
+    // is back again, all the queries will be invalidated.
+    retry: false,
+
+    // The client node should be local, so display the cache value while
+    // making a background request looks good.
+    staleTime: 0,
+
+    // Refreshing when focus returns can be useful if a user comes back
+    // to the UI after performing an operation in the terminal.
+    refetchOnWindowFocus: true,
+
+    // Throw the error to the error boundary
+    throwOnError: true,
+  });
+
+  const onPinAdd = useCallback((pin: PeerPin) => {
+    setPins((val) => {
+      const [, quantity = 0] =
+        val.find(([p]) => p.lat === pin.lat && p.lng == pin.lng) || [];
+      return [...val, [pin, quantity + 1]];
+    });
+  }, []);
+
+  // It’s safe to re-create the map at each render, because of the
+  // pre-computation it’s super fast ⚡️
+  const map = new DottedMap({ map: JSON.parse(mapJsonString) });
+
+  pins.map(([pin, quantity]) =>
+    map.addPin({
+      lat: pin.lat,
+      lng: pin.lng,
+      svgOptions: { color: "#d6ff79", radius: 0.8 * quantity },
+    })
+  );
+
+  const svgMap = map.getSVG({
+    radius: 0.42,
+    color: "#423B38",
+    shape: "circle",
+    backgroundColor: "#020300",
+  });
+
+  const headers = ["Country", "PeerId", "Active"];
+
+  const rows =
+    (data?.table?.nodes || []).map((node) => (
+      <Row
+        cells={[
+          <PeerCountryCell
+            onPinAdd={onPinAdd}
+            address={node.address}></PeerCountryCell>,
+          <Cell>{node.peerId}</Cell>,
+          <Cell>
+            {node.seen ? (
+              <div className="networkIndicator-point networkIndicator-point--online"></div>
+            ) : (
+              <div className="networkIndicator-point networkIndicator-point--offline"></div>
+            )}
+          </Cell>,
+        ]}></Row>
+    )) || [];
+
+  return (
+    <div className="peers">
+      {/* <img
+        src={`data:image/svg+xml;utf8,${encodeURIComponent(svgMap)}`}
+        className="peers-map"
+      /> */}
+
+      <div
+        className="peers-map"
+        dangerouslySetInnerHTML={{ __html: svgMap }}></div>
+
+      <Table headers={headers} rows={rows} className="peers-table" />
+    </div>
+  );
+};
+
 export const Route = createFileRoute("/dashboard/peers")({
-  component: () => {
-    const [pins, setPins] = useState<[PeerPin, number][]>([]);
-    const { data } = useQuery({
-      queryFn: () =>
-        CodexSdk.debug.info().then((s) => Promises.rejectOnError(s)),
-      queryKey: ["debug"],
-
-      // No need to retry because if the connection to the node
-      // is back again, all the queries will be invalidated.
-      retry: false,
-
-      // The client node should be local, so display the cache value while
-      // making a background request looks good.
-      staleTime: 0,
-
-      // Refreshing when focus returns can be useful if a user comes back
-      // to the UI after performing an operation in the terminal.
-      refetchOnWindowFocus: true,
-
-      // Throw the error to the error boundary
-      throwOnError: true,
-    });
-
-    const onPinAdd = useCallback((pin: PeerPin) => {
-      setPins((val) => {
-        const [, quantity = 0] =
-          val.find(([p]) => p.lat === pin.lat && p.lng == pin.lng) || [];
-        return [...val, [pin, quantity + 1]];
-      });
-    }, []);
-
-    // It’s safe to re-create the map at each render, because of the
-    // pre-computation it’s super fast ⚡️
-    const map = new DottedMap({ map: JSON.parse(mapJsonString) });
-
-    pins.map(([pin, quantity]) =>
-      map.addPin({
-        lat: pin.lat,
-        lng: pin.lng,
-        svgOptions: { color: "#d6ff79", radius: 0.8 * quantity },
-      })
-    );
-
-    const svgMap = map.getSVG({
-      radius: 0.42,
-      color: "#423B38",
-      shape: "circle",
-      backgroundColor: "#020300",
-    });
-
-    const headers = ["Country", "PeerId", "Active"];
-
-    const rows =
-      ((data as any)?.table?.nodes || []).map((node: any) => (
-        <Row
-          cells={[
-            <PeerCountryCell
-              onPinAdd={onPinAdd}
-              address={node.address}></PeerCountryCell>,
-            <Cell>{node.peerId}</Cell>,
-            <Cell>
-              {node.seen ? (
-                <div className="networkIndicator-point networkIndicator-point--online"></div>
-              ) : (
-                <div className="networkIndicator-point networkIndicator-point--offline"></div>
-              )}
-            </Cell>,
-          ]}></Row>
-      )) || [];
-
-    return (
-      <div className="peers">
-        {/* <img
-          src={`data:image/svg+xml;utf8,${encodeURIComponent(svgMap)}`}
-          className="peers-map"
-        /> */}
-
-        <div
-          className="peers-map"
-          dangerouslySetInnerHTML={{ __html: svgMap }}></div>
-
-        <Table headers={headers} rows={rows} className="peers-table" />
-      </div>
-    );
-  },
+  component: Peers,
 });
