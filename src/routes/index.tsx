715aeb4796d3831d5ae76d4e0ae0f8286d98c4ac
Arnaud <arnaud@status.im>
2024-10-14 15:30:02 +0200

    Add splash screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,98 @@
 import { createFileRoute, Link, redirect } from "@tanstack/react-router";
+import "./index.css";
+import { AlphaIcon } from "../components/AlphaIcon/AlphaIcon";
+import { AlphaText } from "../components/AlphaText/AlphaText";
+import { SimpleText } from "@codex-storage/marketplace-ui-components";
+import { ArrowRight } from "lucide-react";
+import { CodexLogo } from "../components/CodexLogo/CodexLogo";
+import { ArrowRightCircle } from "../components/ArrowRightCircle/ArrowRightCircle";
+import { useNetwork } from "../network/useNetwork";
+import { NetworkIcon } from "../components/NetworkIcon/NetworkIcon";
 
 export const Route = createFileRoute("/")({
   component: Index,
   beforeLoad: async () => {
-    throw redirect({
-      to: "/dashboard",
-    });
+    // throw redirect({
+    //   to: "/dashboard",
+    // });
   },
 });
 
 function Index() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+  const online = useNetwork();
 
-      <Link to="/dashboard">Go to dashboard</Link>
+  const text = online ? "Network connected" : "Network disconnected";
+
+  return (
+    <div className="index">
+      <div className="index-container">
+        <div className="index-column">
+          <div className="index-column-section">
+            <h1 className="index-title">Codex</h1>
+            <h2 className="index-subtitle">Vault</h2>
+          </div>
+
+          <div className="index-column-section">
+            <div>
+              <AlphaIcon />
+            </div>
+            <div className="index-alphaText">
+              <p>
+                <AlphaText></AlphaText>
+              </p>
+              <p>
+                <SimpleText className="index-version" variant="normal">
+                  {import.meta.env.PACKAGE_VERSION}
+                </SimpleText>
+              </p>
+              <p>
+                <SimpleText className="index-disclaimer" variant="error">
+                  <a className="index-link">Legal Disclaimer</a>
+                </SimpleText>
+              </p>
+            </div>
+          </div>
+          <div className="index-column-section">
+            <h3 className="index-mainTitle">
+              Hello,
+              <br /> Welcome to <span className="index-codex">Codex</span>{" "}
+              <span className="index-vault">Vault</span>
+            </h3>
+            <p className="index-description">
+              <SimpleText variant="light">
+                Codex is a durable, decentralised data storage protocol, created
+                so the world community can preserve its most important knowledge
+                without risk of censorship.
+              </SimpleText>
+            </p>
+          </div>
+          <div className="index-column-section ">
+            <SimpleText variant="primary">
+              <a href="/dashboard" className="index-link index-getStarted">
+                Let’s get started <ArrowRight></ArrowRight>
+              </a>
+            </SimpleText>
+
+            <div className="index-dots">
+              <span className="index-dot index-dot--active"></span>
+              <span className="index-dot"></span>
+              <span className="index-dot"></span>
+            </div>
+          </div>
+        </div>
+        <div className="index-columnRight">
+          <div className="index-logo">
+            <div className="index-network">
+              <p className="index-network-text">{text}</p>
+              <NetworkIcon></NetworkIcon>
+            </div>
+            <CodexLogo></CodexLogo>
+          </div>
+          <a href="/dashboard" className="index-link2">
+            <ArrowRightCircle></ArrowRightCircle>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
