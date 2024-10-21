37c0df08885a9e5e074921b6ee68e5d42270bcbc
Arnaud <arnaud@status.im>
2024-10-21 15:42:59 +0200

    Change echo env url

--- src/utils/echo.ts
+++ src/utils/echo.ts
@@ -1,5 +1,5 @@
 export const Echo = {
-    portForwarding: () => fetch(import.meta.env.VITE_ECHO_URL + "/port/8070")
+    portForwarding: () => fetch(import.meta.env.VITE_GEO_IP_URL + "/port/8070")
         .then((res) => res.json())
 
 }
