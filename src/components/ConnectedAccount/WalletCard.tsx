9edf09bf93ed248f34010cc3406b43b4cbd59049
Arnaud <arnaud@status.im>
2024-11-25 16:37:13 +0100

    Add chart for connected account

--- src/components/ConnectedAccount/WalletCard.tsx
+++ src/components/ConnectedAccount/WalletCard.tsx
@@ -1,15 +1,37 @@
 import "./WalletCard.css";
 import ArrowRightIcon from "../../assets/icons/arrow-right.svg?react";
 import ArrowLeftIcon from "../../assets/icons/arrow-left.svg?react";
-import WalletChart from "../../assets/icons/chart.svg?react";
-import WalletLines from "../../assets/icons/lines.svg?react";
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, useRef, useEffect } from "react";
+
+import * as echarts from "echarts/core";
+import { LineChart } from "echarts/charts";
+import { GridComponent } from "echarts/components";
+import { UniversalTransition } from "echarts/features";
+import { CanvasRenderer } from "echarts/renderers";
 
 const defaultTokenValue = 1540;
 
-export function WalletCard() {
+echarts.use([GridComponent, LineChart, CanvasRenderer, UniversalTransition]);
+
+type Props = {
+  tab: "weekly" | "daily" | "monthly";
+};
+
+export function WalletCard({ tab }: Props) {
   const [tokenValue, setTokenValue] = useState(defaultTokenValue);
   const [currency, setCurrency] = useState("USD");
+  const div = useRef<HTMLDivElement>(null);
+  const chart = useRef<echarts.EChartsType | null>(null);
+  const [, setRefresher] = useState(Date.now());
+
+  useEffect(() => {
+    if (div.current && !chart.current) {
+      chart.current = echarts.init(div.current, null, {
+        renderer: "svg",
+      });
+      setRefresher(Date.now());
+    }
+  }, [chart, div]);
 
   const onCurrencyChange = async (e: ChangeEvent<HTMLSelectElement>) => {
     const value = e.currentTarget.value;
@@ -24,6 +46,51 @@ export function WalletCard() {
       setTokenValue(defaultTokenValue * json.usd.eur);
     }
   };
+
+  if (chart.current) {
+    const data = {
+      daily: ["MON", "TUE", "WED", "THU", "WED", "SAT", "SUN"],
+      weekly: ["1", "2", "3", "4", "5", "6"],
+      monthly: ["JAN", "FEB", "MAR", "APR", "MAY", "JUN"],
+    }[tab];
+
+    const option = {
+      color: ["#3EE089"],
+      xAxis: {
+        type: "category",
+        data: data,
+        show: true,
+        splitLine: {
+          show: true,
+          lineStyle: {
+            width: 0.5,
+            type: "dashed",
+          },
+        },
+      },
+      yAxis: {
+        type: "value",
+        show: false,
+      },
+      series: [
+        {
+          data: [220, 932, 401, 934, 1290, 1330, 1450].slice(0, data.length),
+          type: "line",
+          smooth: true,
+          lineStyle: {
+            normal: {
+              color: "#3EE089",
+            },
+          },
+        },
+      ],
+      tooltip: {
+        type: "item",
+      },
+    };
+
+    chart.current.setOption(option);
+  }
 
   return (
     <div className="wallet-card">
@@ -46,8 +113,9 @@ export function WalletCard() {
         </section>
 
         <section>
-          <WalletChart></WalletChart>
-          <WalletLines></WalletLines>
+          {/* <WalletChart></WalletChart> */}
+          {/* <WalletLines></WalletLines> */}
+          <div className="lines" ref={div} style={{ height: 200 }}></div>
         </section>
       </main>
 
